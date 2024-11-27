b3d795e8d56bdb35cc188d82e041e5ac5eb38cf1
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-11-27 18:42:55 +0600

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
+Route::get('/',HomeController::class)->name('home');
+
+Route::view('services','services')->name('services');
+Route::view('about','about')->name('about');
+Route::view('contact','contact')->name('contact');
